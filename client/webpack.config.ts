ad42e729abcf482f9763fcf390a6dc7dc11f0447
lanre <arsenmahtaev@gmail.com>
2024-04-06 18:33:31 +0300

    webpack fixed

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -1,6 +1,7 @@
     const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './src/index.tsx',
@@ -37,7 +38,12 @@ module.exports = {
             template: path.join(__dirname, 'public', 'index.html'),
             filename: 'index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/images', to: 'images' }
+            ]
+        })
     ],
     devtool: 'source-map',
     resolve: {
